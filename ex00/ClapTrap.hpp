e34e7a51063e85d786d4283109e35a8c68536524
fran <user@users-MacBook-Pro.local>
2025-03-03 12:12:37 +0100

    ex00 done

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap 
+{
+  private:
+    std::string _name;
+    int _hitpoints;
+    int _energypoints;
+    int _attackdamage;
+  
+  public:
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    ClapTrap(std::string name);
+    ClapTrap();
+    ~ClapTrap();
+    ClapTrap(const ClapTrap& claptrap);
+    ClapTrap& operator=(const ClapTrap& claptrap);
+};
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include "ClapTrap.hpp"
 
-int main()
-{
-  std::cout << "Hello World this is a test" << std::endl;
-  return 0;
+int main() {
+    ClapTrap claptrap1;
+    ClapTrap claptrap2("CLTP");
+
+    std::cout << "Initial state of claptrap1:" << std::endl;
+    claptrap1.attack("target1");
+
+    std::cout << "Initial state of claptrap2:" << std::endl;
+    claptrap2.attack("target2");
+
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2");
+    claptrap2.attack("target2"); 
+
+    ClapTrap claptrap3 = claptrap2;
+    claptrap3.attack("target3");
+
+    claptrap1 = claptrap2;
+    claptrap1.attack("target4");
+
+    return 0;
 }
