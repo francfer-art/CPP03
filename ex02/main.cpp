061d8377dd2737bd438668518fa1986276008ce4
Francisco Fernandez Leon <francfer@c3r9s2.42malaga.com>
2025-08-28 18:58:06 +0200

    Final

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,19 +1,3 @@
-// #include <iostream>
-// #include "ClapTrap.hpp"
-// #include "ScavTrap.hpp"
-
-// int main() {
-//     ScavTrap scavtrap1;
-//     ScavTrap scavtrap2("SCVTP");
-//     scavtrap1.attack("SCVTP");
-//     scavtrap1.guardGate();
-//     scavtrap2.guardGate();
-    
-
-
-//     return 0;
-// }
-
 #include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 
